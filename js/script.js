da6b9e4b9d943307415c583efb72fe276de0f54b
Demarrage <gui.vbsamolao@hotmail.com>
2020-10-28 09:53:52 -0300

    adcionamos cards para os produtos

--- js/script.js
+++ js/script.js
@@ -9,11 +9,16 @@ fetch("http://10.26.45.43:4500/listar")
     var acesso = dados[0].navegacao.split(",");
     for (var i = 0; i <= 3; i++) {
       bar.innerHTML += '<li class="nav-item">';
-      bar.innerHTML += '<a hrrf="#" class="nav-link">' + acesso[i] + "</a>";
+      bar.innerHTML +=
+        '<a href="' +
+        acesso[i] +
+        '.html" class="nav-link">' +
+        acesso[i] +
+        "</a>";
       bar.innerHTML += "</li>";
     }
     for (var i = 0; i <= 1; i++) {
-      pr.innerHTML += dados[0].main[i].nome;
+      // pr.innerHTML += dados[0].main[i].nome;
     }
     rd.innerHTML = dados[0].footer;
   })
